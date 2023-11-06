e8e3af259fcb5438f5bf188a700878e6cbdb78dd
zteffano <48279765+zteffano@users.noreply.github.com>
2023-11-06 21:28:56 +0100

    Add project files.

---
+++ Microservice_TrailerTrak/Model/Trailer.cs
@@ -0,0 +1,20 @@
+﻿namespace Microservice_TrailerTrak.Model
+{
+
+    // Trailer Model udkast
+    // Tænker måske vi skal ændre det hele til engelsk, just in case
+    public class Trailer
+    {
+        public int ID { get; set; }
+        public string Navn { get; set; }
+        public int Årgang { get; set; }
+        public string Mærke { get; set; }
+        public int Vægt { get; set; }
+        public int MaxVægt { get; set; }
+        public bool BookedStatus { get; set; }
+        public DateTime? BookedIndtil { get; set; }
+        public decimal PrisDag { get; set; }
+        public decimal PrisUge { get; set; }
+        public decimal PrisMåned { get; set; }
+    }
+}
