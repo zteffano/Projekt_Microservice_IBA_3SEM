65aa800f9488455a6a6d99f3d4b2c112d8f283b5
zimula <zimula@live.dk>
2023-11-07 11:03:32 +0100

    updated booking class

--- Microservice_TrailerTrak/Model/Booking.cs
+++ Microservice_TrailerTrak/Model/Booking.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public DateTime BookedFrom { get; set; }
-        public DateOnly BookedTo { get; set; }
+        public DateTime BookedTo { get; set; }
+        public bool Returned { get; set; }
+        public bool Paid { get; set; }
+        public Customer CustomerData { get; set; }
+        public Trailer TrailerData { get; set; }
     }
 }
