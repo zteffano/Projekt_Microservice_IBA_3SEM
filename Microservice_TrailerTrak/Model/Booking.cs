53eb115a964c1ca302c047bf00d31d367d9d11a6
zimula <zimula@live.dk>
2023-11-07 10:59:32 +0100

    test2

---
+++ Microservice_TrailerTrak/Model/Booking.cs
@@ -0,0 +1,9 @@
+﻿namespace Microservice_TrailerTrak.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime BookedFrom { get; set; }
+        public DateOnly BookedTo { get; set; }
+    }
+}
