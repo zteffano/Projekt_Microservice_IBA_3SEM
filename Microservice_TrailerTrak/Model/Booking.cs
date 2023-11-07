1e1521234bfa7d75d041e6fef95f800ba120f5b6
zimula <zimula@live.dk>
2023-11-07 11:27:41 +0100

    opdaterede booking class

--- Microservice_TrailerTrak/Model/Booking.cs
+++ Microservice_TrailerTrak/Model/Booking.cs
@@ -5,9 +5,11 @@
         public int Id { get; set; }
         public DateTime BookedFrom { get; set; }
         public DateTime BookedTo { get; set; }
+        public int CustomerID { get; set; }
+        public int TrailerId { get; set; }
         public bool Returned { get; set; }
         public bool Paid { get; set; }
-        public Customer CustomerData { get; set; }
-        public Trailer TrailerData { get; set; }
+        public Customer? customer { get; set; }
+        public Trailer? trailer { get; set; }
     }
 }
--- Microservice_TrailerTrak/Model/Customer.cs
+++ Microservice_TrailerTrak/Model/Customer.cs
@@ -3,10 +3,10 @@
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
     }
 }
